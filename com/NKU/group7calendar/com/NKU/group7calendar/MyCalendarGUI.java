6a4a24d9f0ccb88f341f2fc9bfdddd35803a5d7d
Mark Khulenberg <khulenberm1@mymail.nku.edu>
2017-09-28 15:28:37 -0400

    Add files via upload

---
+++ com/NKU/group7calendar/com/NKU/group7calendar/MyCalendarGUI.java
@@ -0,0 +1,56 @@
+package com.NKU.group7calendar;
+
+public class MyCalendarGUI extends MyCalendar {
+
+    int startMonth = 0;
+    int startYear = 0;
+
+    public MyCalendarGUI(int year, int month, int dayOfMonth) {
+        super(year, month, dayOfMonth);
+        startMonth = month;
+        startYear = year;
+    }
+
+    public void nextDay() {
+
+        // if day is at the end of month, change month and reset day
+        if (get(DATE) == daysInMonth()) {
+            nextMonth();
+        }else {
+            set(DATE, get(DATE) + 1);
+        }
+    }
+
+    public void currentMonth() {
+        set(MONTH, startMonth);
+        set(YEAR, startYear);
+    }
+
+    public void nextMonth()
+    {
+        set(DATE, 1);
+        set(MONTH, (get(MONTH) + 1) % 12);
+
+        if (get(MONTH) == 0) {
+            set(YEAR, get(YEAR) + 1);
+        }
+    }
+
+    public void previousMonth(){
+        set(DATE, 1);
+
+        int month = get(MONTH) - 1;
+        if (month < 0) {
+            set(MONTH, 11);
+            set(YEAR, get(YEAR) - 1);
+        } else {
+            set(MONTH, month);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return getMonthName() + " " + get(DATE) + ", " + get(YEAR);
+    }
+}
